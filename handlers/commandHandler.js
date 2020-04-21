149c539fd7ecf6392b6893de85898484163b3ffa
Warpfox <tylermwatts@gmail.com>
2020-04-21 08:30:17 -0500

    add !zgwho list printing command

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -12,6 +12,10 @@ const dbService = require('../services/dbService')
 const zgLoot = require('../data/zg.json')
 const numberReacts = require('../data/numberReacts.json')
 
+const returnFormatedDateString = (hyphenatedDate) => {
+	return moment(hyphenatedDate, 'MM-DD-YYYY').format('dddd, MMMM Do YYYY')
+}
+
 module.exports = async (message) => {
 	switch (message.content) {
 		case '!help':
@@ -20,16 +24,14 @@ module.exports = async (message) => {
 			)
 			message.delete()
 			break
-		case '!zgloot':
+		case '!zgloot': {
+			const channel = message.channel
 			const sentMessage = await message.author.send(
 				`Using the MM-DD-YYYY format including leading zeros (ex: 02-28-2020), please enter the date that this raid will take place. You will have 3 minutes to respond. Invalid date formatting will not be accepted. When a valid date is given in the proper format, you will be asked to confirm this date.`
 			)
-			const filter = (m) => {
-				const index = m.content.search(
-					/^(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|20)\d\d$/
-				)
-				return index >= 0 ? true : false
-			}
+
+			const filter = (m) =>
+				/^(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|20)\d\d$/.test(m.content)
 			const dateCollector = await sentMessage.channel.createMessageCollector(
 				filter,
 				{
@@ -38,64 +40,73 @@ module.exports = async (message) => {
 					errors: ['time'],
 				}
 			)
-			dateCollector.on('collect', async (m) => {
+
+			dateCollector.on('collect', async (message) => {
 				const confirmMessage = await sentMessage.channel.send(
-					`${moment(m.content, 'MM-DD-YYYY').format(
-						'dddd, MMMM Do YYYY'
+					`${returnFormatedDateString(
+						message.content
 					)} is the date that you requested. Is this correct? You have 90 seconds to confirm via reaction to this message. When you confirm, a collection will be created in the database and the loot selection message will be posted in the channel, so please be 100% certain.\n\n:one: Yes\n:two: No`
 				)
-				const filter = (reaction, user) => user.id !== process.env.BOT_ID
-				const collected = await confirmMessage.awaitReactions(filter, {
-					max: 1,
-					time: 90000,
-					errors: ['time'],
-				})
-				confirmMessage.react('1️⃣').then(() => confirmMessage.react('2️⃣'))
-				const reaction = collected.first()
-				if (reaction._emoji.name === '1️⃣') {
-					confirmMessage.channel.send('Date is confirmed.')
-					const lootTable = messageCreator(
-						messageCommands.ZG_LOOT_SELECTION,
-						m.content
-					)
-					message.channel.send(lootTable).then((sentMessage) => {
-						const filter = (reaction, user) => user.id !== process.env.BOT_ID
-						const reactionCollector = sentMessage.createReactionCollector(
-							filter,
-							{
-								maxUsers: 30,
-							}
-						)
-						zgLoot.bosses.forEach((boss) => {
-							sentMessage.react(boss.reaction)
-						})
-						sentMessage.react('❔')
-						sentMessage.react('❌')
-						reactionCollector.on('collect', (reaction, user) => {
-							reactionHandler(reaction, user)
-						})
-					})
-				}
-				if (reaction._emoji.name === '2️⃣') {
-					confirmMessage.channel.send(
-						'Well, you done fucked up buddy. Start over.'
-					)
-				}
-			})
 
+				const filter = (reaction, user) => user.id !== process.env.BOT_ID
+				const dateConfirmCollector = confirmMessage.createReactionCollector(
+					filter,
+					{ max: 1, time: 90000, errors: ['time'] }
+				)
+
+				dateConfirmCollector.on('collect', (reaction, user) => {
+					if (reaction.emoji.name === '1️⃣') {
+						confirmMessage.channel.send('Date is confirmed.')
+
+						const lootTable = messageCreator(
+							messageCommands.ZG_LOOT_SELECTION,
+							m.content
+						)
+
+						channel.send(lootTable).then((sentMessage) => {
+							const filter = (reaction, user) => user.id !== process.env.BOT_ID
+							const reactionCollector = sentMessage.createReactionCollector(
+								filter,
+								{
+									maxUsers: 30,
+								}
+							)
+
+							zgLoot.bosses.forEach((boss) => {
+								sentMessage.react(boss.reaction)
+							})
+
+							sentMessage.react('❔')
+							sentMessage.react('❌')
+
+							reactionCollector.on('collect', (reaction, user) => {
+								reactionHandler(reaction, user)
+							})
+						})
+					}
+
+					if (reaction.emoji.name === '2️⃣') {
+						confirmMessage.channel.send(
+							'Well, you done fucked up buddy. Start over.'
+						)
+					}
+				})
+
+				confirmMessage.react('1️⃣').then(() => confirmMessage.react('2️⃣'))
+			})
 			message.delete()
 			break
+		}
 		case '!zgprint': {
 			const user = message.author
 			const channel = message.channel
 			const events = await dbService.retrieveAllEvents()
 			const eventSelectString = events
 				.map((event, index) => {
-					return `${numberReacts[index]} ${moment(event, 'MM-DD-YYYY').format(
-						'dddd, MMMM Do YYYY'
-					)}`
+					return `${numberReacts[index]} ${returnFormatedDateString(event)}`
 				})
 				.join('\n')
+
 			const sentMessage = await user.send(
 				`The following events were found in the database. Please use reactions to select the event for which you would like to print the list. **This will not cause the list to print. There will be a verification step before the list is printed.**\n\n${eventSelectString}`
 			)
@@ -105,19 +116,22 @@ module.exports = async (message) => {
 				filter,
 				{ time: 180000, errors: ['time'] }
 			)
+
 			eventReactionCollector.on('collect', async (reaction, user) => {
 				const emoji = reaction.emoji.name
 				const eventIndex = _.findKey(numberReacts, (r) => r === emoji)
 				const event = events[eventIndex]
 				const eventVerifyMessage = await user.send(
-					`You have selected the event **${moment(event, 'MM-DD-YYYY').format(
-						'dddd, MMMM Do YYYY'
+					`You have selected the event **${returnFormatedDateString(
+						event
 					)}**.\n\nWhen you react to confirm this, **the full item list will be printed in the public channel** so please be certain you are ready to print the list.\n\n:one: Yes\n:two: No`
 				)
+
 				const eventVerifyReactionCollector = eventVerifyMessage.createReactionCollector(
 					filter,
 					{ time: 180000, errors: ['time'] }
 				)
+
 				eventVerifyReactionCollector.on(
 					'collect',
 					async (verifyReaction, user) => {
@@ -131,9 +145,46 @@ module.exports = async (message) => {
 						}
 					}
 				)
+
 				eventVerifyMessage
 					.react('1️⃣')
 					.then(() => eventVerifyMessage.react('2️⃣'))
+			})
+			events.forEach((e, i) => sentMessage.react(numberReacts[i]))
+			message.delete()
+			break
+		}
+		case '!zgwho': {
+			const user = message.author
+			const events = await dbService.retrieveAllEvents()
+			const eventSelectString = events
+				.map((event, index) => {
+					return `${numberReacts[index]} ${returnFormatedDateString(event)}`
+				})
+				.join('\n')
+			const sentMessage = await user.send(
+				`The following events were found in the database. Please use reactions to select the event for which you would like to see a list of users who have items reserved.\n\n${eventSelectString}`
+			)
+
+			const filter = (reaction, user) => user.id !== process.env.BOT_ID
+			const eventReactionCollector = sentMessage.createReactionCollector(
+				filter,
+				{ max: 1, time: 180000, errors: ['time'] }
+			)
+
+			eventReactionCollector.on('collect', async (reaction, user) => {
+				const emoji = reaction.emoji.name
+				const eventIndex = _.findKey(numberReacts, (r) => r === emoji)
+				const event = events[eventIndex]
+				const eventList = await dbService.retrieveList(event)
+				const usersString = eventList
+					.map((record, index) => `${index + 1}. <@${record.user}>`)
+					.join('\n')
+				user.send(
+					`Here is the list of users who have items reserved for **${returnFormatedDateString(
+						event
+					)}**\n\n${usersString}`
+				)
 			})
 			events.forEach((e, i) => sentMessage.react(numberReacts[i]))
 			message.delete()
